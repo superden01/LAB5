cabe82395812b2064728051a398e41694a0feb10
superden01 <73470502+superden01@users.noreply.github.com>
2020-10-27 19:00:35 +0200

    Add files via upload

---
+++ LAB1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+    
+    std::cout << "Привіт,Александров Денис\n";
+    system("pause");
+
+
+    return 0;
+}
