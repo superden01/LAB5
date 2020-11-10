3632b8d821b7a5618b2288d9c2b64b67cc76812b
“superden01” <superden230576@gmail.com>
2020-11-10 15:53:47 +0200

    add Lab7

---
+++ Lab7.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	int y, n = 3;
+	cout << "Enter y =";
+	cin >> y;
+	float rez;
+	int sum=0 ;
+	for (int n = 1; n < 4; n++)
+	{
+		sum = sum + pow(n, 2);
+	}
+
+	rez =sum*pow(y,3);
+	cout << "Y=" << rez << endl;
+
+	system("pause");
+	return 0;
+}
+
